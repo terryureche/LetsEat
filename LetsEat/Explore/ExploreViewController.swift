700b7044c3d49259e9932c0c13550d354547ca24
Terry <terryureche@gmail.com>
2020-09-08 23:22:43 +0300

    Start project

---
+++ LetsEat/Explore/ExploreViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by Ureche gabriel terry on 08/09/2020.
+//  Copyright © 2020 Ureche Terry. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
